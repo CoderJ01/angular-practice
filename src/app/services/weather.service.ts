608a8ae48872de632ac7ae6c898886fcc5844af7
CoderJ01 <buildingguy@sbcglobal.net>
2023-12-07 19:32:18 -0800

    changes

--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/internal/Observable';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -7,4 +9,12 @@ import { HttpClient } from '@angular/common/http';
 export class WeatherService {
 
   constructor(private http: HttpClient) { }
+
+  // getWeatherData(cityName:string):Observable<WeatherData>{
+  //   return this.http.get<WeatherData>(environment.weatherApiBaseUrl+'/city/'+cityName, {
+  //     headers: new HttpHeaders()
+  //     .set(environment.XRapidAPIHostHeaderName, environment.XRapidAPIHostHeaderValue)
+  //     .set(environment.XRapidAPIKeyHeaderName,environment.XRapidAPIKeyHeaderValue),
+  //   });
+  //  }
 }
